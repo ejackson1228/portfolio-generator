376c43f9c6237efe9f7fcab313c8b8a130614cb6
ejackson1228 <ejackson1228@gmail.com>
2022-09-04 18:27:56 -0400

    generated html file and exported function from page-template to app

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i])
-    }
+const generatePage = require('./src/page-template.js');
 
-    console.log('==========')
+const profileDataArgs = process.argv.slice(2);
 
-    // Is the same as this...
-profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-printProfileData(profileDataArgs)
+const [Name, github] = profileDataArgs;
+
+
+fs.writeFile('./index.html', generatePage(Name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log("portfolio complete! check out index.html too see the output");
+});
